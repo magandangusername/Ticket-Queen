67d2274d1f8c924e0fb4ceb6118e4962a3bdb21b
Matthew <perez_johnmatthewleonard@plpasig.edu.ph>
2022-03-18 18:01:57 +0800

    react practice

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -6,16 +6,103 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\queenticketeventinfo;
 use App\Models\ConcertListing;
+use Exception;
 class ListingController extends Controller
 {
     //
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show()
     {
         $data=queenticketeventinfo::all();
-       
+
 
         return view('listing',['queenticketeventinfo'=>$data]);
-        
+
     }
-   
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        //
+    }
+
+    public function deleteTicket(Request $request)
+    {
+        try
+    {
+        $ids = array_column($request->id, "id");
+        ConcertListing::whereIn('id', $ids)->delete();
+        return response()->json('Enquiry deleted');
+    }
+
+    catch (Exception $e) {
+        return response()->json($e->getMessage(), 500);
+    }
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('projects', 'ProjectController@index');
+Route::post('projects', 'ProjectController@store');
+Route::get('projects/{id}', 'ProjectController@show');
+Route::put('projects/{project}', 'ProjectController@markAsCompleted');
+Route::post('tasks', 'TaskController@store');
+Route::put('tasks/{task}', 'TaskController@markAsCompleted');
