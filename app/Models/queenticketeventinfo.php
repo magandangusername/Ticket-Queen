ac9bba8ace0378e1ce010c4eb8c12bf0ebef70ad
Gobrrtt11 <bordadora_johnmark@plpasig.edu.ph>
2022-03-23 15:45:44 +0800

    remaining days and active set

--- app/Models/queenticketeventinfo.php
+++ app/Models/queenticketeventinfo.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Carbon;
 class queenticketeventinfo extends Model
 {
     use HasFactory;
     protected $table = 'queenticketeventinfo';
+   
+    //get remaining days in specific event
+    public function getRemainingDaysAttribute()
+    {
+    
+        if ($this->ConcertDate) {
+            $remaining_days = Carbon::now()->diffInDays(Carbon::parse($this->ConcertDate));
+        } else {
+            $remaining_days = 0;
+        }
+        return $remaining_days;
+    }
 }
