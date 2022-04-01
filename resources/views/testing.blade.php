e53455bca971fb39a7bef3b7697143cc31402b9e
bluC137 <chumbingco_bluandrei@plpasig.edu.ph>
2022-04-01 16:20:30 +0800

    3+1 modals in testing

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -6,7 +6,7 @@
 
 
 @section('content')
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tiktypes">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#persexpress">
     Open modal
 </button>
 
@@ -274,72 +274,124 @@
                     <br>
                     The O2, London, United Kingdom
                 </p>
-                <table class="container form bg-light text-dark border" >
-                    <div >
+                <table class="container form bg-light text-dark border">
+                    <div>
                         <tr>
-                        <th>Section</th>
-                        <th>Row</th>
-                        <th>Seat From</th>
-                        <th>Seat To</th>
-                        <th>Website Price</th>
-                        <th>Proceeds</th>
-                        <th>Available Ticket</th>
-                        <th></th>
+                            <th>Section</th>
+                            <th>Row</th>
+                            <th>Seat From</th>
+                            <th>Seat To</th>
+                            <th>Website Price</th>
+                            <th>Proceeds</th>
+                            <th>Available Ticket</th>
+                            <th></th>
                         </tr>
                         <tr class="p-2">
                             <td>
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                        <i class="fa-regular fa-chevron-down"></i>
-                                        <option selected>Choose one</option>
-                                        <option>Lounge</option>
-                                        <option>417</option>
-                                        <option>Lower Tier</option>
-                                        <option>Upper Tier</option>
-                                    </select>
+                                <select class="form-control" id="exampleFormControlSelect1">
+                                    <i class="fa-regular fa-chevron-down"></i>
+                                    <option selected>Choose one</option>
+                                    <option>Lounge</option>
+                                    <option>417</option>
+                                    <option>Lower Tier</option>
+                                    <option>Upper Tier</option>
+                                </select>
                             </td>
                             <td>
-                            <select class="form-control" id="exampleFormControlSelect1">
-                                        <option>A</option>
-                                        <option>B</option>
-                                        <option selected>P</option>
-                                        <option>C</option>
-                                        <option>D</option>
-                                        <i class="fa-regular fa-chevron-down"></i>
-                                    </select>
-                                </div>
-                            </td>
-                            <td>
-                            <input type="seatfrom" class="form-control" id="exampleFormControlInput1">
-                            </td>
-                            <td>
-                            <input type="seatto" class="form-control" id="exampleFormControlInput1">
-                            </td>
-                            <td>
-                            <input type="webprice" class="form-control" id="exampleFormControlInput1">
-                            </td>
-                            <td>
-                            <input type="proceeds" class="form-control" id="exampleFormControlInput1">
-                            </td>
-                            <td>
-                            <input type="availtick" class="form-control" id="exampleFormControlInput1">
-                            </td>
-                            <td class="text-justify"><i class="fa text-danger fa-times-circle" aria-hidden="true"></i></td>
-                        </tr>
-
+                                <select class="form-control" id="exampleFormControlSelect1">
+                                    <option>A</option>
+                                    <option>B</option>
+                                    <option selected>P</option>
+                                    <option>C</option>
+                                    <option>D</option>
+                                    <i class="fa-regular fa-chevron-down"></i>
+                                </select>
                     </div>
-                </table>
+                    </td>
+                    <td>
+                        <input type="seatfrom" class="form-control" id="exampleFormControlInput1">
+                    </td>
+                    <td>
+                        <input type="seatto" class="form-control" id="exampleFormControlInput1">
+                    </td>
+                    <td>
+                        <input type="webprice" class="form-control" id="exampleFormControlInput1">
+                    </td>
+                    <td>
+                        <input type="proceeds" class="form-control" id="exampleFormControlInput1">
+                    </td>
+                    <td>
+                        <input type="availtick" class="form-control" id="exampleFormControlInput1">
+                    </td>
+                    <td class="text-justify"><i class="fa text-danger fa-times-circle" aria-hidden="true"></i></td>
+                    </tr>
 
+            </div>
+            </table>
+
+
+        </div>
+        <!-- Modal footer -->
+        <div class="modal-footer m-1 p-2">
+
+            <button type="button" class="btn btn-light">+ Add another clone</button>
+
+            <button type="button" class="btn btn-light">Save</button>
+
+            <button type="button" class="btn btn-secondary">Cancel</button>
+
+        </div>
+
+    </div>
+</div>
+</div>
+
+<div class="modal container-fluid" id="persexpress">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content text-center">
+            <!-- Modal Header -->
+            <div class="modal-header m-1 p-2">
+
+                <b>
+                    <h4 class="modal-title">Select Ticket Type</h4>
+                </b>
+
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+
+            <!-- Modal body -->
+            <div class="form-group modal-body text-left m-3 p-3">
+                <p class="row ">
+                    
+                   By opting in to Express Local Shipping your listings will stay active for longer,giving you a higher chance of selling your tickets.
+                </p>
+                <p class="row">
+                    Tickets for events in the same city as the Ticket Location – the physical location of your ticket – will be available for Express Local Shipping, allowing them to be sold to nearby buyers as close as 4 hours before the event Make sure you’re aware of which locations your listings are registered to – only the tickets at locations in the same city as the event will stay active for longer!
+                </p>
+                <p class="row">
+                    If your tickets sell close to the event (in the sales window limited to Express Local Shipping only), orders will be automatically confirmed and you should be prepared to contact the local courier we recommend using within one hour of the sale, for immediate pick up.
+                </p>
+                <p class="row">
+                    If you are no longer able to dispatch orders this quickly, you must opt out of Express Local Shipping or unpublish you listings or you may incur charges.
+                  
+                </p>
+                <p class="row">
+                    You will be able to view instructions on how to arrange the courier with each order. You will have to pay for the courier to make the delivery, and will receive a reimbursement when you receive the proceeds for the sale. 
+                </p>
 
             </div>
             <!-- Modal footer -->
             <div class="modal-footer m-1 p-2">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Do not show this again
+                    </label>
+                </div>
 
-                <button type="button" class="btn btn-light">+ Add another clone</button>
-
-                <button type="button" class="btn btn-light">Save</button>
-
-                <button type="button" class="btn btn-secondary">Cancel</button>
-
+                <div>
+                    <button type="button" class="btn btn-success">Confirm</button>
+                </div>
             </div>
 
         </div>
