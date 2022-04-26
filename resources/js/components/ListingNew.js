b1b0088cf5034bfcb523039fa71099f16995212a
Matthew <perez_johnmatthewleonard@plpasig.edu.ph>
2022-04-26 13:51:30 +0800

    working interaction for new listing searching

--- resources/js/components/ListingNew.js
+++ resources/js/components/ListingNew.js
@@ -1,15 +1,20 @@
 import React from "react";
-import dateFormat from 'dateformat';
+import dateFormat from "dateformat";
 
-const ListingNew = ({ concerts, handleTicketNew }) => {
+const ListingNew = ({
+    concerts,
+    handleTicketNew,
+    newListingSearch,
+    setNewListingSearch,
+}) => {
     return (
         <div className="modal w-100" id="ListingModal">
             <div className="modal-dialog">
                 <div className="modal-content">
                     {/* <!-- Modal Header --> */}
                     <div
-                    className="modal-header p-2"
-                    style={{ background: "#424549", color: "#edf6ff"}}
+                        className="modal-header p-2"
+                        style={{ background: "#424549", color: "#edf6ff" }}
                     >
                         <h4>Create New Listing</h4>
 
@@ -28,6 +33,13 @@ const ListingNew = ({ concerts, handleTicketNew }) => {
                                 className="form-control"
                                 name=""
                                 placeholder="Search by Event, Venue, City"
+                                value={newListingSearch.search}
+                                onChange={(e) =>
+                                    setNewListingSearch({
+                                        ...newListingSearch,
+                                        search: e.target.value,
+                                    })
+                                }
                             />
                         </div>
                         <form action="" className="row">
@@ -39,6 +51,17 @@ const ListingNew = ({ concerts, handleTicketNew }) => {
                                     type="date"
                                     id="date"
                                     name="date"
+                                    value={newListingSearch.from}
+                                    onChange={(e) =>
+                                        setNewListingSearch({
+                                            ...newListingSearch,
+                                            from: e.target.value,
+                                        })
+                                    }
+                                    min={dateFormat(
+                                        new Date(),
+                                        "yyyy-mm-dd"
+                                    )}
                                 />
                             </div>
 
@@ -51,31 +74,99 @@ const ListingNew = ({ concerts, handleTicketNew }) => {
                                     type="date"
                                     id="date"
                                     name="date"
+                                    value={newListingSearch.to}
+                                    onChange={(e) =>
+                                        setNewListingSearch({
+                                            ...newListingSearch,
+                                            to: e.target.value,
+                                        })
+                                    }
+                                    min={dateFormat(
+                                        new Date(),
+                                        "yyyy-mm-dd"
+                                    )}
                                 />
                             </div>
                         </form>
                         <table className="table">
                             {concerts.length ? (
                                 <tbody>
-                                    {concerts.map((concert) => (
-                                        <tr key={concert.event_id} data-bs-toggle="modal" data-bs-target="#tiktypes" onClick={()=>handleTicketNew(concert)}>
-                                            <td>
-                                            {/* dateFormat("2019-04-30T08:59:00.000Z", "dddd, mmmm dS, yyyy") to get Tuesday, April 30th, 2019. */}
-                                                {dateFormat(concert.event_date, "dddd")}
-                                                <br />
-                                                {dateFormat(concert.event_date, "dd, mmmm yyyy")}
-                                                <br />
-                                                {/* 19:00 */}
-                                            </td>
-                                            <td>
-                                                {concert.event_name}
-                                                <br />
-                                                {concert.event_venue}
-                                                <br />
-                                                {/* Nanterre */}
-                                            </td>
-                                        </tr>
-                                    ))}
+                                    {concerts.map((concert) =>
+                                        ((concert.event_name.toLowerCase().includes(newListingSearch.search.toLowerCase()) |
+                                        concert.event_venue.toLowerCase().includes(newListingSearch.search.toLowerCase()) |
+                                        concert.event_city.toLowerCase().includes(newListingSearch.search.toLowerCase())
+                                        ) & (
+                                        (newListingSearch.from !== "" ?
+                                        new Date(concert.event_date) >=
+                                            new Date(newListingSearch.from):true)
+                                        &
+                                        (newListingSearch.to !== "" ?
+                                        new Date(concert.event_date) <=
+                                            new Date(newListingSearch.to):true))) ? (
+                                            <tr
+                                                key={concert.event_id}
+                                                data-bs-toggle="modal"
+                                                data-bs-target="#tiktypes"
+                                                onClick={() =>
+                                                    handleTicketNew(concert)
+                                                }
+                                            >
+                                                <td>
+                                                    {/* dateFormat("2019-04-30T08:59:00.000Z", "dddd, mmmm dS, yyyy") to get Tuesday, April 30th, 2019. */}
+                                                    {dateFormat(
+                                                        concert.event_date,
+                                                        "dddd"
+                                                    )}
+                                                    <br />
+                                                    {dateFormat(
+                                                        concert.event_date,
+                                                        "dd, mmmm yyyy"
+                                                    )}
+                                                    <br />
+                                                    {/* 19:00 */}
+                                                </td>
+                                                <td>
+                                                    {concert.event_name}
+                                                    <br />
+                                                    {concert.event_city},{" "}
+                                                    {concert.event_venue}
+                                                    <br />
+                                                    {/* Nanterre */}
+                                                </td>
+                                            </tr>
+                                        ) : newListingSearch.from === "" & newListingSearch.from === ""? (
+                                            <tr
+                                                key={concert.event_id}
+                                                data-bs-toggle="modal"
+                                                data-bs-target="#tiktypes"
+                                                onClick={() =>
+                                                    handleTicketNew(concert)
+                                                }
+                                            >
+                                                <td>
+                                                    {dateFormat(
+                                                        concert.event_date,
+                                                        "dddd"
+                                                    )}
+                                                    <br />
+                                                    {dateFormat(
+                                                        concert.event_date,
+                                                        "dd, mmmm yyyy"
+                                                    )}
+                                                    <br />
+                                                    {/* 19:00 */}
+                                                </td>
+                                                <td>
+                                                    {concert.event_name}
+                                                    <br />
+                                                    {concert.event_city},{" "}
+                                                    {concert.event_venue}
+                                                    <br />
+                                                    {/* Nanterre */}
+                                                </td>
+                                            </tr>
+                                        ) : null
+                                    )}
                                 </tbody>
                             ) : null}
                             {/* <tbody>
